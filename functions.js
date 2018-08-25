4738da937f2182d1755dae4f0b2d5a386c90d53b
Ripal Patel <ripal212007@gmail.com>
2018-08-25 11:49:17 -0500

    made writeToDom

--- functions.js
+++ functions.js
@@ -8,6 +8,7 @@ function nuggetizer(animal){
 console.log('fish:', nuggetizer('fish'));
 console.log('cat:', nuggetizer('cat'));
 console.log('bear:', nuggetizer('bear'));
+
 const nuggetizer2 = (animal) => {
     return `${animal}jerky`
 };
@@ -19,3 +20,18 @@ const nomnom = (person, food) => {
 
 const bearSticks = nuggetizer('bear');
 console.log(nomnom('zoe', bearSticks)); //zoe devoured bearstix
+
+// function numberadder that takes in a number and return the number plus 3. so input=22, output = 25
+
+const numberAdder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+};
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+numberAdder(22);
+numberAdder(3);
